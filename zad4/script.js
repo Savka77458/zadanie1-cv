bc3da6a6a3c8ea4e8afbaefd4cd2172d4bfa48f7
Vladyslav Savka <vsavka1@stu.vistula.edu.pl>
2026-04-09 14:35:31 +0200

    Dodanie skryptu JavaScript | PL77458

--- zad4/script.js
+++ zad4/script.js
@@ -1,1 +1,23 @@
+// Numer indeksu: PL77458
 
+const themeBtn = document.getElementById('themeBtn');
+const themeStyle = document.getElementById('theme-style');
+
+themeBtn.addEventListener('click', function() {
+    if (themeStyle.getAttribute('href') === 'red.css') {
+        themeStyle.setAttribute('href', 'green.css');
+    } else {
+        themeStyle.setAttribute('href', 'red.css');
+    }
+});
+
+const sectionBtn = document.getElementById('sectionBtn');
+const sectionToToggle = document.getElementById('toggle-section');
+
+sectionBtn.addEventListener('click', function() {
+    if (sectionToToggle.style.display === 'none') {
+        sectionToToggle.style.display = 'block';
+    } else {
+        sectionToToggle.style.display = 'none';
+    }
+});
